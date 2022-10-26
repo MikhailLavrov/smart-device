c05ec784fb9fe13e9dd7b0ced384d96984323f83
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-26 18:31:23 +0300

    modal is ready

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,11 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 
-// ---------------------------------
-
 window.addEventListener('DOMContentLoaded', () => {
-
   // Utils
-  // ---------------------------------
-
   iosVhFix();
-
   // Modules
   // ---------------------------------
-
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
