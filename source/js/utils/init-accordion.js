e1372df1c288e77509f1241730749b4d2c9481e5
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-27 18:31:16 +0300

    add no-js cond for accordions

--- source/js/utils/init-accordion.js
+++ source/js/utils/init-accordion.js
@@ -2,6 +2,8 @@ import {Accordions} from './accordions';
 let accordions;
 
 const initAccordions = () => {
+  document.querySelectorAll('.is-active').forEach((el) => el.classList.remove('is-active'));
+
   accordions = new Accordions();
   // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
   window.accordions = accordions;
