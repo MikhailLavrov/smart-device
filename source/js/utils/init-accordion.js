8e77f33197f74b0c563c15946f1b45df5376f902
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-27 22:46:33 +0300

    int conditions

--- source/js/utils/init-accordion.js
+++ source/js/utils/init-accordion.js
@@ -1,8 +1,11 @@
 import {Accordions} from './accordions';
+
+const ACTIVE_ACCORDION_CLASSNAME = 'is-active';
 let accordions;
+let activeElements = document.querySelectorAll('.is-active');
 
 const initAccordions = () => {
-  document.querySelectorAll('.is-active').forEach((el) => el.classList.remove('is-active'));
+  activeElements.forEach((el) => el.classList.remove(ACTIVE_ACCORDION_CLASSNAME));
 
   accordions = new Accordions();
   // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
