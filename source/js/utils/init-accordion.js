19f122b78efce67620238f9b5d497ebc0349bb4d
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-10-28 12:27:07 +0300

    retina css

--- source/js/utils/init-accordion.js
+++ source/js/utils/init-accordion.js
@@ -1,11 +1,11 @@
 import {Accordions} from './accordions';
 
-const ACTIVE_ACCORDION_CLASSNAME = 'is-active';
+const NOJS_ACCORDION_CLASSNAME = 'is-active';
 let accordions;
 let activeElements = document.querySelectorAll('.is-active');
 
 const initAccordions = () => {
-  activeElements.forEach((el) => el.classList.remove(ACTIVE_ACCORDION_CLASSNAME));
+  activeElements.forEach((el) => el.classList.remove(NOJS_ACCORDION_CLASSNAME));
 
   accordions = new Accordions();
   // Используйте в разработке экспортируемую переменную accordions, window сделан для бэкэнда
